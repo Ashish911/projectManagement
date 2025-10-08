e68d36cfde5a66492548d3d192ef5c7564b8d618
Ashish911 <ashishdongol32@gmail.com>
2025-10-08 21:08:20 +0100

    Implementing Dashboard component.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
-import { Login } from "./Screens/Login";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
-import { Register } from "./Screens/Register";
+import { Login } from "./Screens/Login.tsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Register } from "./Screens/Register.tsx";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { store } from "@/redux/store/store.ts"
+import store from "@/redux/store/store.ts"
 import {Provider} from "react-redux";
+import { Dashboard } from "@/Screens/Dashboard.tsx";
 
 const queryClient = new QueryClient();
 
@@ -20,6 +21,7 @@ function App() {
                 path={"/register"}
                 element={<Register currentLanguage="en" />}
               />
+              <Route path="/dashboard" element={<Dashboard currentLanguage="en" />} />
             </Routes>
           </div>
         </Router>
